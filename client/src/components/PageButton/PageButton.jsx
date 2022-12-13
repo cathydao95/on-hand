461f3462ff9e4f1b2eb1b46b673b58fc72514c1c
Cathy Dao <cathydao95@gmail.com>
2022-12-12 22:19:06 -0800

    pagination and styling

---
+++ client/src/components/PageButton/PageButton.jsx
@@ -0,0 +1,113 @@
+import { useState, useContext } from "react";
+import styles from "./styles.module.scss";
+import RecipeContext from "../../context/recipe_context";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+
+const PageButton = () => {
+  const { numOfPages, page, changePage, limit, changeLimit } =
+    useContext(RecipeContext);
+
+  const [selectPage, setSelectPage] = useState(page);
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+
+  const nextPage = () => {
+    let newPage = page + 1;
+    if (newPage > numOfPages) {
+      newPage = 1;
+    }
+    changePage(newPage);
+  };
+  const prevPage = () => {
+    let newPage = page - 1;
+    if (newPage < 1) {
+      newPage = numOfPages;
+    }
+    changePage(newPage);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    changePage(selectPage);
+  };
+
+  console.log(selectPage);
+  return (
+    <div className={styles.containerWrapper}>
+      <button className={styles.prevBtn} onClick={prevPage}>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <form className={styles.inputContainer} onSubmit={(e) => handleSubmit(e)}>
+        <span>Page</span>
+        <input
+          className={styles.input}
+          type="number"
+          value={selectPage}
+          onChange={(e) => setSelectPage(e.target.value)}
+          min="1"
+          max={numOfPages}
+        ></input>
+        <span>of</span>
+        {numOfPages}
+
+        <select
+          className={styles.select}
+          value={limit}
+          size="4"
+          onChange={(e) => changeLimit(e.target.value)}
+        >
+          <option value="12" className={styles.option}>
+            12
+          </option>
+          <option value="24">24</option>
+          <option value="48">48</option>
+          <option value="60">60</option>
+        </select>
+      </form>
+      {/* <div className={styles.btnContainer}>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              type="button"
+              key={pageNumber}
+              onClick={() => changePage(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div> */}
+      <button className={styles.nextBtn} onClick={nextPage}>
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </div>
+    //   <div className={styles.containerWrapper}>
+    //   <button className={styles.prevBtn} onClick={prevPage}>
+    //     <HiChevronDoubleLeft />
+    //     prev
+    //   </button>
+    //   <div className={styles.btnContainer}>
+    //     {pages.map((pageNumber) => {
+    //       return (
+    //         <button
+    //           type="button"
+    //           key={pageNumber}
+    //           onClick={() => changePage(pageNumber)}
+    //         >
+    //           {pageNumber}
+    //         </button>
+    //       );
+    //     })}
+    //   </div>
+    //   <button className={styles.nextBtn} onClick={nextPage}>
+    //     next
+    //     <HiChevronDoubleRight />
+    //   </button>
+    // </div>
+  );
+};
+
+export default PageButton;
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,79 @@
+import { useEffect, useState } from "react";
+import SingleRecipe from "../SingleRecipe/SingleRecipe";
 import styles from "./styles.module.scss";
+import clsx from "clsx";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useContext } from "react";
+import RecipeContext from "../../context/recipe_context";
+import PageButton from "../../components/PageButton/PageButton";
+import { UserContext } from "../../context/user_context";
+import { IoLogOut } from "react-icons/io5";
+
+// import { useRecipeContext } from "../../context/recipe_context";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [userInput, setUserInput] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const [expand, setExpand] = useState(false);
+
+  const { user } = useContext(UserContext);
+  const { recipes, getAllRecipes, isLoading, page, limit, totalRecipes } =
+    useContext(RecipeContext);
+
+  useEffect(() => {
+    getAllRecipes();
+  }, [page, limit]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (recipes.length === 0) {
+    return <div>No Recipes to Display</div>;
+  }
+
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={clsx(styles.title, "title")}>
+        <h1>All Recipes</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.formControl}>
+          {/* <AiOutlineSearch className={styles.searchIcon} /> */}
+          <input
+            className={styles.searchInput}
+            type="text"
+            placeholder="Type your ingredients, separated by commas"
+            onChange={(e) => handleChange(e)}
+            value={userInput}
+          />
+          <button className={styles.searchBtn}>
+            <AiOutlineSearch className={styles.searchIcon} />
+          </button>
+        </div>
+      </form>
+
+      <div className={styles.test2}>
+        <div className={styles.recipesContainer}>
+          {recipes.map((ele, index) => {
+            return (
+              <div className={styles.singleRecipeContainer} key={index}>
+                <SingleRecipe key={index} {...ele} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <PageButton />
+    </div>
+  );
 };
 export default Dashboard;

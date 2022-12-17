2ae04262dfaffa5e110c32c828ae0c236c2b0b59
Cathy Dao <cathydao95@gmail.com>
2022-12-16 20:41:42 -0800

    update nav and footer styling

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -10,13 +10,45 @@ const Navbar = () => {
   const [showDropDown, setShowDropDown] = useState(false);
   const { logOutUser, user } = useContext(UserContext);
 
-  console.log(showDropDown);
   return (
     <nav className={styles.navContainer}>
       <div>
         <h1 className={styles.title}>On Hand</h1>
       </div>
-
+      <div className={styles.navLinks}>
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            isActive ? styles.linkActive : styles.link
+          }
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="/recipes"
+          className={({ isActive }) =>
+            isActive ? styles.linkActive : styles.link
+          }
+        >
+          Search
+        </NavLink>
+        <NavLink
+          to="/favorites"
+          className={({ isActive }) =>
+            isActive ? styles.linkActive : styles.link
+          }
+        >
+          Favorites
+        </NavLink>
+        <NavLink
+          to="/create"
+          className={({ isActive }) =>
+            isActive ? styles.linkActive : styles.link
+          }
+        >
+          Create Recipe
+        </NavLink>
+      </div>
       {user && (
         <div className={styles.accountContainer}>
           <button
@@ -38,76 +70,6 @@ const Navbar = () => {
           </div>
         </div>
       )}
-      {/* <div className={styles.navContainer}>
-        <div className={styles.navCenter}>
-          
-          <h1>On Hand</h1>
-        </div>
-
-        <div className={styles.navLinks}>
-          <NavLink
-            to="/dashboard"
-            className={({ isActive }) =>
-              isActive ? styles.linkActive : styles.link
-            }
-          >
-            Dashboard
-          </NavLink>
-          <NavLink
-            to="/recipes"
-            className={({ isActive }) =>
-              isActive ? styles.linkActive : styles.link
-            }
-          >
-            Search
-          </NavLink>
-          <NavLink
-            to="/favorites"
-            className={({ isActive }) =>
-              isActive ? styles.linkActive : styles.link
-            }
-          >
-            Favorites
-          </NavLink>
-          <NavLink
-            to="/create"
-            className={({ isActive }) =>
-              isActive ? styles.linkActive : styles.link
-            }
-          >
-            Create Recipe
-          </NavLink>
-        </div>
-        {user && (
-          <div className={styles.accountContainer}>
-            <div className={styles.accountWrapper}>
-              <button
-                type="button"
-                onClick={() => setShowDropDown(!showDropDown)}
-                className={styles.accountBtn}
-              >
-                <FaUserCircle />
-                {user?.name}
-                <FaCaretDown />
-              </button>
-              <div
-                className={showDropDown ? styles.showDropDown : styles.dropDown}
-              >
-                <NavLink className={styles.settingsBtn} to="/account">
-                  Account
-                </NavLink>
-                <button
-                  type="button"
-                  className={styles.btn}
-                  onClick={logOutUser}
-                >
-                  logout
-                </button>
-              </div>
-            </div>
-          </div>
-        )} */}
-      {/* </div> */}
     </nav>
   );
 };

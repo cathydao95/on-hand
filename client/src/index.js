2a7c9f7579c125098fd6c5dfbc0a9ae5cd2dce81
Cathy Dao <cathydao95@gmail.com>
2022-12-16 22:31:19 -0800

    update css and add userprovide

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import styles from "./styles/index.scss";
 import App from "./App";
+import { UserProvider } from "./context/user_context";
 import { RecipeProvider } from "./context/recipe_context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RecipeProvider>
-    <App />
-  </RecipeProvider>
+  <UserProvider>
+    <RecipeProvider>
+      <App />
+    </RecipeProvider>
+  </UserProvider>
 );

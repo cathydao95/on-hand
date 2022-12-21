25a616f85a6c892bb8df46abbbec6eb6b353a429
Cathy Dao <cathydao95@gmail.com>
2022-12-20 20:55:11 -0800

    update alerts

--- client/src/components/Alert/Alert.jsx
+++ client/src/components/Alert/Alert.jsx
@@ -1,15 +1,11 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../context/user_context";
-import styles from "./styles.modules.scss";
 
 const Alert = () => {
   const { alertText, alertType } = useContext(UserContext);
+
   return (
-    <div
-      className={
-        alertType === "danger" ? styles.alertDanger : styles.alertSuccess
-      }
-    >
+    <div className={alertType === "danger" ? "alertDanger" : "alertSuccess"}>
       {alertText}
     </div>
   );
